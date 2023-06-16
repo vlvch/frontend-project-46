0152bd56ad7250ed4112d530b405cd20fa98939f
vlvch <107064521+vlvch@users.noreply.github.com>
2023-06-16 11:22:14 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -2,7 +2,8 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<type>', 'output format')
+  .option('-f, --format <type>', 'output information')
 
 program.parse();
-  
