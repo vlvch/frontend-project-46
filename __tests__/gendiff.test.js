dce55f8159ff0c4ee8498432367dd71bc4dbe7fd
vlvch <107064521+vlvch@users.noreply.github.com>
2023-07-16 19:37:42 +0300

    fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,25 +12,35 @@ const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), { encoding: 'utf8' });
 
 const table = [
-  { file1: 'file1.json', file2: 'file2.json', resultFile: 'result1.txt' },
-  { file1: 'file1.yml', file2: 'file2.yml', resultFile: 'result1.txt' },
-  { file1: 'file1.yaml', file2: 'file2.yaml', resultFile: 'result1.txt' },
-  { file1: 'file1.yaml', file2: 'file2.json', resultFile: 'result1.txt' },
   {
-    file1: 'file1.json', file2: 'file2.json', resultFile: 'result2.txt', format: 'plain',
+    name: 'extension json', file1: 'file1.json', file2: 'file2.json', resultFile: 'result1.txt',
   },
   {
-    file1: 'file1.json', file2: 'file2.json', resultFile: 'result1.txt', format: 'stylish',
+    name: 'extension yml', file1: 'file1.yml', file2: 'file2.yml', resultFile: 'result1.txt',
   },
   {
-    file1: 'file1.json', file2: 'file2.json', resultFile: 'result3.txt', format: 'json',
+    name: 'extension json', file1: 'file1.yaml', file2: 'file2.yaml', resultFile: 'result1.txt',
+  },
+  {
+    name: 'extension json and yaml', file1: 'file1.yaml', file2: 'file2.json', resultFile: 'result1.txt',
+  },
+  {
+    name: 'format plain', file1: 'file1.json', file2: 'file2.json', resultFile: 'result2.txt', format: 'plain',
+  },
+  {
+    name: 'format stylish', file1: 'file1.json', file2: 'file2.json', resultFile: 'result1.txt', format: 'stylish',
+  },
+  {
+    name: 'format json', file1: 'file1.json', file2: 'file2.json', resultFile: 'result3.txt', format: 'json',
   },
 ];
 
-test.each(table)('test', ({
-  file1, file2, resultFile, format,
+describe.each(table)('test', ({
+  name, file1, file2, resultFile, format,
 }) => {
-  const result = readFile(resultFile);
+  test(`${name}`, () => {
+    const result = readFile(resultFile);
 
-  return expect(genDiff(file1, file2, format)).toEqual(result);
+    return expect(genDiff(file1, file2, format)).toEqual(result);
+  });
 });
